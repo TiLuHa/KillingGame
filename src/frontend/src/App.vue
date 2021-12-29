89d695f9e7882d87b118ec0a2e637e153af7af05
Tim Lukas Harken <tlh@tlharken.de>
2021-12-30 00:19:31 +0100

    Add Frontend

---
+++ src/frontend/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <HelloWorld />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'Killing Game',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+
+</style>
